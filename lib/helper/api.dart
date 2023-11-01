5274f619d801cfb7bd6ee92ecd0e5fc70779e723
Riyam224 <cs50riyam211#gmail.com>
2023-11-01 13:50:39 +0300

    add put request

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -37,4 +37,30 @@ class Api {
           'error status code ${response.statusCode} with body ${jsonDecode(response.body)}');
     }
   }
+
+  // todo put request
+  Future<dynamic> put(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    // todo
+    headers.addAll(
+      {'Content-Type': 'application/x-www-form-urlencoded'},
+    );
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post((Uri.parse(url)), body: body, headers: headers);
+    // todo check for status code exception
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      // throw Exception(jsonDecode(response.body));
+      throw Exception(
+          'error status code ${response.statusCode} with body ${jsonDecode(response.body)}');
+    }
+  }
 }
