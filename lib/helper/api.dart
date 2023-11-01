151377d4eb57467049248febe29ca6c7d3a74506
Riyam224 <cs50riyam211#gmail.com>
2023-11-01 13:17:44 +0300

    add post request to api.dart

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,7 +1,11 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Api {
+  // todo get request
   Future<dynamic> get({required String url}) async {
     http.Response response = await http.get(Uri.parse(url));
 
@@ -10,5 +14,20 @@ class Api {
     } else {
       throw Exception('error status code ${response.statusCode}');
     }
+  }
+
+  // todo post request
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post((Uri.parse(url)), body: body, headers: headers);
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data;
   }
 }
